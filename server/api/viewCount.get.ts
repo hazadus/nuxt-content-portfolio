81a656c716a56a456a6aee557b9370cc24491a19
hazadus <hazadus7@gmail.com>
2023-06-30 11:04:09 +0300

    Added USE_REDIS env var as a flag to turn off Redis in dev mode.

--- server/api/viewCount.get.ts
+++ server/api/viewCount.get.ts
@@ -1,6 +1,9 @@
 export default defineEventHandler(async (event) => {
   //
   // Increase and get view count from Redis for page with `pageId`.
+  // Set `USE_REDIS` env var to True when you run the app alongside Redis instance.
+  // Otherwise, this endpoint will always return 0 to work around connection errors when there's
+  // no Redis instance running.
   //
   // Usage:
   //
@@ -11,15 +14,15 @@ export default defineEventHandler(async (event) => {
   const redisKey: string = `viewCount:${pageId}`;
   let viewCount = 1;
 
-  const redis = useStorage("redis");
-
-  if (redis) {
+  if (process.env.USE_REDIS) {
+    const redis = useStorage("redis");
     const data = await redis.getItem(redisKey);
 
     if (data) viewCount = Number(data.toString()) + 1;
-
     await redis.setItem(redisKey, viewCount);
-  }
 
-  return viewCount;
+    return viewCount;
+  } else {
+    return 0;
+  }
 });
