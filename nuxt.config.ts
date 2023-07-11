bc1c4f619a560ba91ca1ac683b01638a70961677
hazadus <hazadus7@gmail.com>
2023-07-11 20:51:56 +0300

    config: updated favicon stuff.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -19,6 +19,33 @@ export default defineNuxtConfig({
           "data-website-id": "765ffad5-0ba1-4925-87e1-d181762834c9",
         },
       ],
+      link: [
+        {
+          rel: "apple-touch-icon",
+          sizes: "180x180",
+          href: "/apple-touch-icon.png",
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "32x32",
+          href: "/favicon-32x32.png",
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "16x16x",
+          href: "/favicon-16x16.png",
+        },
+        { rel: "manifest", href: "/site.webmanifest" },
+        { rel: "mask-icon", href: "/safari-pinned-tab.svg", color: "#5bbad5" },
+      ],
+      meta: [
+        { name: "apple-mobile-web-app-title", content: "Hazadus.ru" },
+        { name: "application-name", content: "Hazadus.ru" },
+        { name: "msapplication-TileColor", content: "#da532c" },
+        { name: "theme-color", content: "#ffffff" },
+      ],
     },
   },
   nitro: {
