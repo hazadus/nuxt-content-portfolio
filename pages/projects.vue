91cb5a1f22f5662a7e2e00d171ea403cd109ad6b
hazadus <hazadus7@gmail.com>
2023-10-12 15:01:16 +0300

    content: Added RSS Reader to Projects page.

--- pages/projects.vue
+++ pages/projects.vue
@@ -21,19 +21,26 @@ const breadcrumbs = [
     запущены и благополучно работают.
   </p>
 
-  <!-- Trololo -->
+  <!-- RSS Reader -->
   <section class="bg-white">
     <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
-      <div class="mr-auto place-self-center lg:col-span-7">
+      <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
+        <img
+          src="/images/rss-reader.png"
+          alt="RSS Reader screenshot"
+        />
+      </div>
+      <div class="pl-4 mr-auto place-self-center lg:col-span-7">
         <h1 class="max-w-2xl mb-4 text-4xl font-bold tracking-tight leading-none md:text-5xl xl:text-6xl">
-          Трололо
+          RSS Reader 🏗️
         </h1>
-        <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl">
-          Удобный и современный инструмент для создания канбан-досок онлайн. Трололо поможет вам качественно
-          спланировать выполнение любых задач, и с удовольсвием довести свои идеи до реализации.
+        <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-4 md:text-lg lg:text-xl">
+          Очередной проект в разработке – онлайн читалка RSS лент. Подписывайтесь на обновление материалов
+          любимых сайтов, и будьте в курсе последних новостей!
         </p>
-        <a
-          href="/blog/nuxt-board-deploy"
+
+        <NuxtLink
+          to="/blog/new-project-rss-reader"
           class="inline-flex items-center justify-center px-5 py-3 mr-2 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100"
         >
           Подробнее
@@ -49,7 +56,47 @@ const breadcrumbs = [
               clip-rule="evenodd"
             ></path>
           </svg>
+        </NuxtLink>
+
+        <a
+          href="http://rss.hazadus.ru/"
+          class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100"
+        >
+          Открыть сайт
         </a>
+      </div>
+    </div>
+  </section>
+
+  <!-- Trololo -->
+  <section class="bg-white">
+    <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+      <div class="mr-auto place-self-center lg:col-span-7">
+        <h1 class="max-w-2xl mb-4 text-4xl font-bold tracking-tight leading-none md:text-5xl xl:text-6xl">
+          Трололо
+        </h1>
+        <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl">
+          Удобный и современный инструмент для создания канбан-досок онлайн. Трололо поможет вам качественно
+          спланировать выполнение любых задач, и с удовольсвием довести свои идеи до реализации.
+        </p>
+        <NuxtLink
+          to="/blog/nuxt-board-deploy"
+          class="inline-flex items-center justify-center px-5 py-3 mr-2 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100"
+        >
+          Подробнее
+          <svg
+            class="w-5 h-5 ml-2 -mr-1"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+              clip-rule="evenodd"
+            ></path>
+          </svg>
+        </NuxtLink>
         <a
           href="http://boards.hazadus.ru/"
           class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100"
@@ -89,8 +136,8 @@ const breadcrumbs = [
         </p>
 
         <!--
-        <a
-          href="#"
+        <NuxtLink
+          to="#"
           class="inline-flex items-center justify-center px-5 py-3 mr-2 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100"
         >
           Подробнее
@@ -106,7 +153,7 @@ const breadcrumbs = [
               clip-rule="evenodd"
             ></path>
           </svg>
-        </a>
+        </NuxtLink>
         -->
         <a
           href="http://bookmarks.hazadus.ru/"
@@ -134,8 +181,8 @@ const breadcrumbs = [
         </p>
 
         <!--
-        <a
-          href="/blog/nuxt-board-deploy"
+        <NuxtLink
+          to="/blog/nuxt-board-deploy"
           class="inline-flex items-center justify-center px-5 py-3 mr-2 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100"
         >
           Подробнее
@@ -151,7 +198,7 @@ const breadcrumbs = [
               clip-rule="evenodd"
             ></path>
           </svg>
-        </a>
+        </NuxtLink>
         -->
         <a
           href="https://github.com/hazadus/journal"
@@ -163,7 +210,7 @@ const breadcrumbs = [
       <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
         <img
           src="/images/journal.png"
-          alt="Boards screenshot"
+          alt="Journal screenshot"
         />
       </div>
     </div>
